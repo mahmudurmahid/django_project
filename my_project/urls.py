4d95e24c813f627b88adeb4c07e0bd77d9b00869
mahmudurmahid <m.mahid@icloud.com>
2025-02-20 17:49:53 +0000

    views, settings, and urls bugs fixed

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from helllo_world import views as index_views
+from hello_world import views as index_views
 
 urlpatterns = [
-    path('', index.views.index, name='index'),
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
