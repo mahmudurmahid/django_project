2985c5b68e7beb24fb5616a9bf0b1c2ecb5ce164
mahmudurmahid <m.mahid@icloud.com>
2025-02-23 13:59:17 +0000

    code for deployment

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault('DEBUG', '1')
--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(#-(+ghc+%9smdj_%!4#*xv2i5r#yak%pxstyg7cx8fom_3xmh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
